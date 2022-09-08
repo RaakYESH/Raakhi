bd5e8ca251c1491459714ad0c9f3a6a213a616a7
raman <raman@DESKTOP-S0303EU>
2022-09-08 14:21:56 +0530

    1st commit

---
+++ src/RaakhiTest1.java
@@ -0,0 +1,21 @@
+
+public class RaakhiTest1 {
+
+	public static void main(String[] args) {
+		
+		int i=10;
+		
+		do{
+			System.out.println("value of i: " +i);
+			i++;
+		}
+		while (i<=15);
+			
+		}
+		
+		
+		
+
+	}
+
+
